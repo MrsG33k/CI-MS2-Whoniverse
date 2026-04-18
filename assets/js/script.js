45bb24635aa8d90a6bfcb61beabf693aaddf4dec
Jennie Whittaker <jenniekwhittaker@gmail.com>
2026-04-18 19:53:01 +0100

    Edit: Added initial JS game load script

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,44 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+// THE GAME
+
+let allLocations = [];
+let currentLocation = null;
+let score = 0;
+let round = 1;
+
+//Fetch the data
+async function loadGameData() {
+    try {
+        const response = await fetch('assets/js/locations.json');
+        allLocations = await response.json();
+        startGame();
+    } catch (error) {
+        console.error("Time Vortex Error: Could not load locations", error);
+    }  
+}
+
+//Start The Game
+function startGame(){
+    //pick a random location
+    const randomIndex = Math.floor(Math.random() * allLocations.length);
+    currentLocation = allLocations[randomIndex];
+    updateDisplay();
+}
+
+//Update game.html
+function updateDisplay(){
+    const mysteryImg = document.getElementById('mystery-image');
+    const roundTxt = document.getElementById('round-count');
+
+    if (currentLocation) {
+        mysteryImg.src = currentLocation.image;
+        mysteryImg.alt = "Mystery Doctor Who Location";
+        roundTxt.innerText = '${round} of 5';
+    }
+}
+
+//Run game on page load
+window.onload = loadGameData;
