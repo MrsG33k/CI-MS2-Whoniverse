60a270daa15b4cdb9305ba66cf74cff7d0d3d7fe
Jennie Whittaker <jenniekwhittaker@gmail.com>
2026-04-28 21:15:38 +0100

    Fix: Added css / JS to enable scroll of modal on mobile.

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,11 +8,13 @@ const closeSpan = document.getElementsByClassName("close-btn")[0];
 //Open Modal on click
 btn.onclick = function() {
     modal.style.display = "block";
+    document.body.classList.add('no-scroll');
 };
 
 //Close Modal on X click
 closeSpan.onclick = function() {
     modal.style.display = "none";
+    document.body.classList.add('no-scroll');
 };
 
 //Close Modal on click outside of modal
